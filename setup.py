4cece6a449de280ef5c3feb10a387dbd92c43712
kochj02 <jonathan.koch@topazlabs.com>
2024-11-19 17:33:39 +0000

    update pkg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="deepseek_vl",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=['./*']),
     install_requires=[
         "transformers==4.46.0",
