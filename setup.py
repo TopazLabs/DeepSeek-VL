dfae2f410d516146f0fe54fa02715403c8dc7741
kochj02 <jonathan.koch@topazlabs.com>
2024-11-19 17:47:52 +0000

    attempt to fix issues

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="deepseek_vl",
     version="0.1.1",
-    packages=find_packages(include=['./*']),
+    packages=find_packages(),
     install_requires=[
         "transformers==4.46.0",
         "torch",
